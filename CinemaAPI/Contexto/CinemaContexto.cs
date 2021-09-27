e2a5b95c843da9b63f288ac2d8b0e4c06313143e
Joao Paulo <paulosantos1799@outlook.com>
2021-09-27 14:50:24 -0300

    features

---
+++ CinemaAPI/Contexto/CinemaContexto.cs
@@ -0,0 +1,25 @@
+﻿using CinemaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaAPI.Contexto
+{
+    public class CinemaContexto : DbContext 
+    {
+
+
+        public CinemaContexto(DbContextOptions<CinemaContexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Bar> Bar { get; set; }
+        public DbSet<Filmes> Filmes { get; set; }
+        public DbSet<Ingressos> Ingressos { get; set; }
+        public DbSet<Programacao> Programacao { get; set; }
+        public DbSet<Sala> Sala { get; set; }
+    }
+}
