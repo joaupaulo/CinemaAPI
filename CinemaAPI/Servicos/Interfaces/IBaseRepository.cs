61ddebd4759ccf3d7772b898c31fe0408e7749ba
Joao Paulo <paulosantos1799@outlook.com>
2021-09-27 23:08:06 -0300

    algumas att

---
+++ CinemaAPI/Servicos/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaAPI.Servicos.Interfaces
+{
+   public interface IBaseRepository<T> where T : class
+    {
+        IEnumerable<T> PegarTodos();
+        T PegarItem(T Id);
+        void Inserir(T obj);
+        void Atualizar(T obj);
+        void Delete(object Id);
+
+    }
+}
---
+++ CinemaAPI/Servicos/Repositorio/BaseRepositorio.cs
@@ -0,0 +1,36 @@
+﻿using CinemaAPI.Servicos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaAPI.Servicos.Repositorio
+{
+    public class BaseRepositorio<T> : IBaseRepository<T> where T : class
+    {
+        public void Atualizar(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T PegarItem(T Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> PegarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
